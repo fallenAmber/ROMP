c751a3d2c0cdae4b88946638a832b0a91fa7d293
Md. Tohidul Islam <48941639+fallenAmber@users.noreply.github.com>
2023-01-29 13:30:11 +0600

    Add files via upload

---
+++ header-secure.php
@@ -0,0 +1,27 @@
+<?php include 'server.php' ?>
+<html>
+	<head>
+		<title>InfoDesk RUET</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/w3.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</head>
+	<body class="subpage">
+	<header id="header">
+				<div class="inner">
+					<a href="start.php" class="logo"><i class="fa fa-spinner w3-spin" style="font-size:24px"></i><strong>InfoDesk</strong> RUET</a>
+					
+					<nav id="nav">
+						<a href="generic.php">Insert Record</a>
+						<a href="contact.php">Contact Us</a>						
+						<a href="header-secure.php?logout" style="color:red;">Logout</a>
+						
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
